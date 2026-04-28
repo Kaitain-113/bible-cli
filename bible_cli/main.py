e374df7754ed8f01af74fa42e985321269fe0192
Kaitain-113 <kaetaen@tutanota.com>
2026-04-27 23:04:35 -0300

    feat: implements database models and connection

--- bible_cli/main.py
+++ bible_cli/main.py
@@ -1,10 +1,15 @@
 import typer
 
+from bible_cli.database import Verse, get_session
+
 app = typer.Typer()
+
 
 @app.command()
 def about():
-    print('✝️ BIBLE_CLI is simple cli app to query bible text from many bible portuguese versions')
+    print(
+        "✝️ BIBLE_CLI is simple cli app to query bible text from many bible portuguese versions"
+    )
 
 
 @app.command()
@@ -12,16 +17,17 @@ def query(q: str):
     """
     Get a text using query:
     [book] [chapter]:[verse/range] [version]
-    
+
     Examples:
         Eclesiastes 3:15
         Eclesiastes 3:15-16
         Eclesiastes 3:15 ARA
     """
+    session = get_session()
+    verses = session.query(Verse).all()
+    for v in verses:
+        print(v.text)
 
-    # @TODO: implementar data source para buscar no banco 
-    
-    print(q)
 
 if __name__ == "__main__":
     app()
